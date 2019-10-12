cfaea747a8038c8b0b9e6abc918f38cbbd3ab85b
bhatku <madhuranand@live.in>
2019-10-12 14:47:48 +0530

    fellow
    f

--- assets/js/main.js
+++ assets/js/main.js
@@ -350,3 +350,12 @@
 	});
 
 })(jQuery);
+
+const items = document.querySelectorAll(".accordion a");
+
+function toggleAccordion(){
+  this.classList.toggle('active');
+  this.nextElementSibling.classList.toggle('active');
+}
+
+items.forEach(item => item.addEventListener('click', toggleAccordion));
